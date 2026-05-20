1fa6709d1b4a17ea706d0281fd709371d279a0c8
tixi333 <tramirezgarin@alumno.huergo.edu.ar>
2026-05-20 15:30:19 -0300

    comentarios pq me mareo

--- bubble sort vs selection sort/insertionsort.py
+++ bubble sort vs selection sort/insertionsort.py
@@ -1,11 +1,17 @@
 def insertionSort(arr):
     for i in range(1, len(arr)):
-        numC = arr[i]
-        j = i - 1
+        numC = arr[i] #numero a comparar
+        j = i - 1 # posicion del numero a la izquierda (numI)
+        #mientras la posicion no sea menor a 0
+        # y numc sea menor a numI
         while j >= 0 and numC < arr[j]:
-            arr[j +1] = arr[j]
-            j -= 1
-        arr[j + 1] = numC
+            arr[j +1] = arr[j] #es menor asi que se mueve a la derecha
+            j -= 1 #siguiente posicion a la izquierda
+        
+        # cuando numI sea menor o igual a numC
+        # se coloca numC a la derecha de numI
+        
+        arr[j + 1] = numC 
     
     return arr
         
