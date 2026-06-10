d740b2145e1e2a44e132cfa079e1dd340d84f781
tr <tramirezgarin@alumno.huergo.edu.ar>
2026-06-10 11:43:39 -0300

    Update linkedlist.py

--- linked list/linkedlist.py
+++ linked list/linkedlist.py
@@ -12,7 +12,6 @@ class singlyLinkedList:
         
         if self.head == None:
             self.head = new_node #pointer/
-            print(new_node.data)
         
         else:
             current = self.head
@@ -20,19 +19,110 @@ class singlyLinkedList:
                 current = current.pointer
             
             current.pointer = new_node
-            print(new_node.data)
             
-        def search_node(self):
-            pass
+    def search_node(self, value):
+                
+        if self.head == None:
+            return "No values defined yet"
         
-        def sort_linkedlist(self):
-            pass
+        else:
+            current = self.head
+            while current.pointer != None:
+                mem = current.pointer
+                current = current.pointer
             
+            if current.data == value:
+                return f"Required value: {value} is located at: {mem}"
+                
+                        
+    def sort_linkedlist(self):
+        if self.head == None:
+            return "No enough nodes to sort"
+        
+        else:
+            pass
+    
+    def delete_node(self,value):
+        
+        if self.head == None: # si no hay elementos 
+            return "No nodes in linked list"
+        
+        elif self.head == value: # si el head es el que se desea eliminar
+            self.head = self.head.pointer
+
+        else:
+            current = self.head
+
+            while current.pointer is not None:
+                if current.pointer.data == value: 
+                    current.pointer = current.pointer.pointer 
+                    return "Deleted"
+                current = current.pointer
+
+    def is_empty(self):
+        if self.head == None:
+            return "True"
+        else:
+            return "False"
+
+    def print_linkedlist(self):
+        lista = []
+        current = self.head
+        while current != None:
+            lista.append(current.data)
+            print(current.data)
+            current = current.pointer
+        
+        return lista
+        
+    def get_size(self):
+        contador = 0
+        if self.head == None:
+            return contador
+
+        else:
+            contador = 1
+            current = self.head
+            while current.pointer != None:
+                current = current.pointer
+                contador +=1
+        
+        return f"linked list has {contador} nodes"
+    
+    def insert_node(self, value, previous):
+        new_node = Nodo(value)
+        current = self.head
+
+        while current is not None:
+            if current.data == previous:
+                new_node.pointer = current.pointer
+                current.pointer = new_node
+                return "inserted"
+
+            current = current.pointer
+            
+            
+
 singly = singlyLinkedList()
+
+print(f"is empty:", singly.is_empty())
+singly.sort_linkedlist()
+print(singly.delete_node(3))
+
+print(singly.search_node(3))
 
 singly.create_newnode(2)
 singly.create_newnode(4)
 singly.create_newnode(5)
+
+print(singly.insert_node(3,2))
+print(singly.get_size())
+
+print(singly.print_linkedlist())
+
+print(singly.delete_node(4))
+print(singly.search_node(5))
+print(singly.is_empty())
 
 # -----------------------------------------------------------------------------
 
@@ -61,11 +151,12 @@ class doubleLinkedList:
         current.pointer = new_node
         new_node.previous = current
 
+"""""
 double  = doubleLinkedList()    
 double.create_newnode(2)
 double.create_newnode(4)
 double.create_newnode(5)        
-        
+"""       
 # -----------------------------------------------------------------------------
 
 class Nodo3:
